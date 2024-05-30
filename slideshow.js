d94a5351002e87c98415e14493aceb3495e7f27b
arn12cyb <kaliarnuad3@gmail.com>
2024-05-30 13:33:34 +0200

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,45 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function plusSlides(n) {
+  slideIndex += n;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex < 1) {slideIndex = slides.length}
+  showSlideManually(slideIndex);
+}
+
+function currentSlide(n) {
+  slideIndex = n;
+  showSlideManually(slideIndex);
+}
+
+function showSlideManually(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[n-1].style.display = "block";  
+  dots[n-1].className += " active";
+}
